157af58f1b2fe1a1732f8d909c078fb71c959017
mpariente <pariente.mnl@gmail.com>
2018-07-20 10:10:58 +0200

    Avoid division by zero in stoi.stoi

--- pystoi/stoi.py
+++ pystoi/stoi.py
@@ -89,8 +89,8 @@ def stoi(x, y, fs_sig, extended=False):
         x_segments = x_segments - np.mean(x_segments, axis=2, keepdims=True)
 
         # Divide by their norms
-        y_primes /= np.linalg.norm(y_primes, axis=2, keepdims=True)
-        x_segments /= np.linalg.norm(x_segments, axis=2, keepdims=True)
+        y_primes /= (np.linalg.norm(y_primes, axis=2, keepdims=True) + utils.EPS)
+        x_segments /= (np.linalg.norm(x_segments, axis=2, keepdims=True) + utils.EPS)
 
         # Find a matrix with entries summing to sum of correlations of vectors
         correlations_components = y_primes * x_segments
