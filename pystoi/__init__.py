9128a35b7142a54b0965191a8488167def08b821
mpariente <pariente.mnl@gmail.com>
2020-03-06 09:39:11 +0100

    Release v0.3.1 on PyPI

--- pystoi/__init__.py
+++ pystoi/__init__.py
@@ -1,3 +1,3 @@
 from .stoi import stoi
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 from setuptools import find_packages
 
+with open("README.md", encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name='pystoi',
-    version='0.3.0',
+    version='0.3.1',
     description='Computes Short Term Objective Intelligibility measure',
     author='Manuel Pariente',
     author_email='pariente.mnl@gmail.com',
     url='https://github.com/mpariente/pystoi',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='MIT',
     install_requires=['numpy', 'scipy'],
     classifiers=[
